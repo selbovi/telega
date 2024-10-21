8b48d31e49853910fcd3e7e164b4899fd24f6f3a
Салават Муфаззалов <s.mufazzalov@soccard.ru>
2024-10-21 15:03:56 +0300

    initial structure

---
+++ src/test/java/com/selbovi/telega/TelegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selbovi.telega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
